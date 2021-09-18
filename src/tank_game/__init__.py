aede3f576f2dbdf4d7e0306dd332a5836a7fdbae
Keenan Gugeler <me@kgugeler.ca>
2021-09-18 15:52:36 -0400

    add settings files and database outline

--- src/tank_game/__init__.py
+++ src/tank_game/__init__.py
@@ -1,11 +1,18 @@
+from celery import Celery
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(
     __name__,
     template_folder="../frontend/templates",
     static_folder="../frontend/static",
 )
+app.config.from_object("tank_game.settings")
+app.config.from_object("tank_game.local_settings")
+
 CORS(app)
 
-app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024
+celery = Celery(app.import_name)
+
+db = SQLAlchemy(app)
---
+++ src/tank_game/database/aliases.py
@@ -0,0 +1,4 @@
+dbmodel = db.Model
+dbcol = db.Column
+dbint = db.Integer
+dbstr = db.String
