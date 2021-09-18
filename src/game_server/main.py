d28a654b2e923a7f6633e2288dcccdbf5bd9c1a4
tmanti <tmanti17@gmail.com>
2021-09-18 11:22:10 -0400

    concept of teams is no more and create folders

---
+++ src/game_server/main.py
@@ -0,0 +1,33 @@
+import sys
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def serve_root():
+    return "Hello, World!", 200
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    port = 3000
+    debug = False
+    while arguments:
+        arg = arguments.pop(0)
+        if arg == "-d" or arg == "--debug":
+            debug = True
+        elif arg == "-p" or arg == "--port":
+            if arguments:
+                rawport = arguments.pop(0)
+                if rawport.isdigit():
+                    port = int(rawport)
+                else:
+                    raise SystemExit(
+                        "Argument after --port / -p must be a positive integer."
+                    )
+            else:
+                raise SystemExit("There must be an argument after --port / -p")
+    app.run(host="0.0.0.0", port=port, debug=debug)
