21c5b5946804ddbe47a08d168ea9adf496c6de82
Keenan Gugeler <me@kgugeler.ca>
2021-09-18 16:54:59 -0400

    add password to users table

--- src/tank_game/database/aliases.py
+++ src/tank_game/database/aliases.py
@@ -1,4 +1,7 @@
+from tank_game import db
+
 dbmodel = db.Model
 dbcol = db.Column
 dbint = db.Integer
 dbstr = db.String
+dbbinary = db.Binary
--- src/tank_game/database/users.py
+++ src/tank_game/database/users.py
@@ -4,3 +4,4 @@ from .aliases import *
 class Users(dbmodel):
     id = dbcol(dbint, primary_key = True)
     username = dbcol(dbstr(256), unique = True, nullable = False)
+    password = dbcol(dbbinary, nullable = False)
