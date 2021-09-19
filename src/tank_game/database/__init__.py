71b8797555c123ee4757476cb62c56f3133ce230
Keenan Gugeler <me@kgugeler.ca>
2021-09-19 00:35:01 -0400

    add frame updates

--- src/tank_game/database/__init__.py
+++ src/tank_game/database/__init__.py
@@ -2,6 +2,7 @@ from tank_game import db
 
 from .users import Users
 from .match import Match
+from .frame_updates import FrameUpdates
 from .match_frame import MatchFrame
 from .match_tanks import MatchTanks
 from .tank_frame import TankFrame
