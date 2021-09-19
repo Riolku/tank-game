795798dbc161980e49b0abb68d86aa5253374f5b
Keenan Gugeler <me@kgugeler.ca>
2021-09-19 01:54:44 -0400

    rudimentary api

--- src/tank_game/game_server/api.py
+++ src/tank_game/game_server/api.py
@@ -3,7 +3,11 @@ import submission
 
 print(json.dumps(submission.get_tanks()))
 
+class Tank:
+    def __init__(self, dt):
+        self.__dict__.update(**dt)
+
 while 1:
     data = json.loads(input())
 
-    print(json.dumps(submission.run_frame(data)))
+    print(json.dumps(submission.run_frame([Tank(tk) for tk in data])))
