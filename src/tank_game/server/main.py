5e6af7703745601ceab34f9e7f58a7366f5c2548
tmanti <tmanti17@gmail.com>
2021-09-19 09:26:34 -0400

    replace my work with alex's and fix query for userlist

--- src/tank_game/server/main.py
+++ src/tank_game/server/main.py
@@ -112,7 +112,7 @@ def handle_signout_request():
 
 @app.route("/userlist")
 def serve_userlist():
-    return render("userlist.html", users=Users.query.all())
+    return render("userlist.html", users=Users.query.filter(Users.code!=None).all())
 
 
 @app.route("/challenge/<int:id>")
@@ -364,16 +364,6 @@ def match_data(mid):
     #         ],
     #     ]
     # )
-
-@app.route("/users")
-def handle_users():
-    users = Users.query.filter(Users.code!=None).all()
-    print(users)
-    return render("users.html", users = users), 200
-
-@app.route("/battle", methods=["POST"])
-def handle_battle():
-    pass
 
 @app.errorhandler(404)
 def not_found(e):
